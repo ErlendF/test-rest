da817a1aa91171c553ab831126ce39362ec28190
Erlend Fonnes <erlend.fonnes@gmail.com>
2019-10-26 16:29:47 +0200

    Minor fix

--- pkg/models/comment.go
+++ pkg/models/comment.go
@@ -3,6 +3,6 @@ package models
 //Comment is a simple comment on a post
 type Comment struct {
 	ID      int64  `json:"id" db:"id"`
-	Post    int64  `json:"-" db:"post"`
+	Post    int64  `json:"post,omitempty" db:"post"`
 	Content string `json:"content" db:"content"`
 }
