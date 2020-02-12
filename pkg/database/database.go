cd8396dc2fab2d65acbb084373866f9726cb8468
Erlend Fonnes <erlend.fonnes@gmail.com>
2020-02-12 15:18:02 +0100

    Fixes to mysql

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"time"
 
 	_ "github.com/go-sql-driver/mysql" // MySQL driver
 	"github.com/jmoiron/sqlx"
@@ -15,37 +14,30 @@ import (
 // Database contains a sql db
 type Database struct {
 	*sqlx.DB
+	dbType string
 }
 
 // New returns a database object
 func New(dbType string) (*Database, error) {
-	db := &Database{}
+	db := &Database{dbType: dbType}
 	var err error
-	var dbOpen *sqlx.DB
+	var info string
 
 	switch dbType {
 	case "postgres":
-		dbOpen, err = openPostgreSQL()
+		info = postgreSQLInfo()
 	case "mysql":
-		dbOpen, err = openMySQL()
+		info = mySQLInfo()
 	default:
 		return nil, fmt.Errorf("not supported database type: %s", dbType)
 	}
+
+	logrus.Infof("Trying to connect to db: %s", info)
+	db.DB, err = sqlx.Connect(dbType, info)
 	if err != nil {
 		return nil, err
 	}
 
-	err = dbOpen.Ping()
-	for i := 0; i < 30 && err != nil; i++ {
-		time.Sleep(time.Duration(i*2) * time.Second)
-		logrus.Warnf("could not ping database, retrying:")
-		err = dbOpen.Ping()
-	}
-	if err != nil {
-		return nil, fmt.Errorf("could not connect to database: %v", err)
-	}
-
-	db.DB = dbOpen
 	maxOpen := 20
 	maxIdle := 15
 
@@ -65,19 +57,17 @@ func New(dbType string) (*Database, error) {
 	return db, nil
 }
 
-func openMySQL() (*sqlx.DB, error) {
+func mySQLInfo() string {
 	host := os.Getenv("DB_HOST")
 	port := os.Getenv("DB_PORT")
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
 	dbname := os.Getenv("DB_NAME")
-	uri := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, password, host, port, dbname)
 
-	logrus.Debugf("Trying to connect to db: %s", uri)
-	return sqlx.Open("mysql", uri)
+	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", user, password, host, port, dbname)
 }
 
-func openPostgreSQL() (*sqlx.DB, error) {
+func postgreSQLInfo() string {
 	host := os.Getenv("DB_HOST")
 	port := os.Getenv("DB_PORT")
 	user := os.Getenv("DB_USER")
@@ -85,8 +75,6 @@ func openPostgreSQL() (*sqlx.DB, error) {
 	dbname := os.Getenv("DB_NAME")
 	sslmode := os.Getenv("DB_SSLMODE")
 
-	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
 		host, port, user, password, dbname, sslmode)
-	logrus.Debugf("Trying to connect to db: %s", psqlInfo)
-	return sqlx.Open("postgres", psqlInfo)
 }
